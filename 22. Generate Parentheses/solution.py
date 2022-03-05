616645902b45f822d9c70b051cd94541d74aa665
Julius Yuan-Chen Huang <medicjue@gmail.com>
2022-03-05 20:30:30 +0800

    22. Generate Partentheses

---
+++ 22. Generate Parentheses/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        return self.g([""], n*2, 0)
+    
+    def g(self, sc: List[str], t: int, cs: int) -> List[str]:
+        if t == 0:
+            return sc
+        if cs == 0:
+            sc[0] = sc[0]+"("
+            return self.g(sc, t-1, cs+1)
+        elif cs == t:
+            sc[0] = sc[0]+")"
+            return self.g(sc, t-1, cs-1)
+        else:
+            split_sc = sc[:]
+            sc[0] = sc[0]+"("
+            split_sc[0] = split_sc[0]+")"
+            return self.g(sc, t-1, cs+1) + self.g(split_sc, t-1, cs-1)
+            
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # leetcode
+
+| Index | Topic | Solution | Difficulty | Acceptance Date |
+| ---- | ---- | ---- | ---- | ---- |
+| [22] (https://leetcode.com/problems/generate-parentheses/) | 22. Generate Parentheses | [python] () | Medium | 2022/03/05 |
